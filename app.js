8fecff1ff7d6436070b1a96087420559487e3370
Waqarghori <waqar.iqbal789@gmail.com>
2025-03-16 00:17:45 +0500

    upd

--- app.js
+++ app.js
@@ -7,24 +7,44 @@ let hour = document.getElementById("hour");
 let min = document.getElementById("min");
 let sec = document.getElementById("sec");
 
+let interval ;
+document.getElementById("stop-btn").disabled = true
 
 function start() {
-    setInterval(function () {
+   interval = setInterval(function () {
         {
             jsSec++
-            sec.innerHTML = jsSec
+            sec.innerHTML = `Seconds: ${jsSec}`;
             if (jsSec >= 59) {
                 jsMin++
-                min.innerHTML = jsMin
+                min.innerHTML = `Minutes: ${jsMin}`
                 jsSec = 0;
             }else if(jsMin >= 9){
                 jsHour++
-                hour.innerHTML = jsHour;
+                hour.innerHTML = `Hours: ${jsHour}`;
                 jsMin = 0
             }
         }
+        document.getElementById("start-btn").disabled = true
+        document.getElementById("stop-btn").disabled = false
+
     }, 10)
 }
 function stop(){
-    
+    clearInterval(interval)
+    document.getElementById("start-btn").disabled = false
+    document.getElementById("stop-btn").disabled = true
+}
+
+function reset(){
+    clearInterval(interval)
+    jsSec = 0;
+    jsMin= 0;
+    jsHour= 0;
+    sec.innerHTML = jsSec;
+    min.innerHTML = jsMin;
+    hour.innerHTML = jsHour;
+    document.getElementById("stop-btn").disabled = true
+    document.getElementById("start-btn").disabled = false
+
 }
