d57fbf983b77698dca4b3789272e517f6fe0a2db
Waqarghori <waqar.iqbal789@gmail.com>
2025-03-18 00:20:47 +0500

    upd

--- app.js
+++ app.js
@@ -1,48 +1,64 @@
 let jsHour = 0;
 let jsMin = 0;
 let jsSec = 0;
+let jsMsec = 0;
 
 
 let hour = document.getElementById("hour");
 let min = document.getElementById("min");
 let sec = document.getElementById("sec");
+let mSec = document.getElementById("msec");
 
-let interval ;
-//document.getElementById("stop-btn").style.visibility = "hidden"
+let interval;
 
 function start() {
-   interval = setInterval(function () {
+    interval = setInterval(function () {
         {
-            jsSec++
-            sec.innerHTML = jsSec;
+            jsMsec++
+            mSec.innerHTML = jsMsec;
+
+            if (jsMsec == 100) {
+                jsSec++
+                sec.innerHTML = jsSec;
+                jsMsec = 0;
+            }
             if (jsSec >= 59) {
                 jsMin++
                 min.innerHTML = jsMin;
                 jsSec = 0;
-            }else if(jsMin >= 9){
+            } else if (jsMin >= 9) {
                 jsHour++
                 hour.innerHTML = jsHour;
                 jsMin = 0
             }
         }
-
-        document.getElementById("start-btn").outerHTML = `<button id="stop-btn" onclick="stop()">Pause</button>`
+        let startBtn = document.getElementById("start-btn");
+        if (startBtn) {
+            startBtn.innerHTML = "Pause";
+            startBtn.setAttribute("onclick", "stop()");
+            startBtn.id = "stop-btn";
+        }
+        //document.getElementById("start-btn").outerHTML = `<button id="stop-btn" onclick="stop()">Pause</button>`
 
     }, 10)
 }
-function stop(){
+
+function stop() {
     clearInterval(interval)
     document.getElementById("stop-btn").outerHTML = `<button id="start-btn" onclick="start()">Start</button>`
 
 }
 
-function reset(){
+function reset() {
     clearInterval(interval)
-    jsSec = 0;
-    jsMin= 0;
-    jsHour= 0;
+    jsMsec = "00";
+    jsSec = "00";
+    jsMin = "00";
+    jsHour = "00";
+    mSec.innerHTML = jsMsec
     sec.innerHTML = jsSec;
     min.innerHTML = jsMin;
     hour.innerHTML = jsHour;
+     document.getElementById("stop-btn").outerHTML = `<button id="start-btn" onclick="start()">Start</button>`
 }
 
